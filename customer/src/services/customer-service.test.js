81dd0964ad56894071642ad6609f0d30f1dffaad
Aikant Kumar <kr.ashok0786@gmail.com>
2025-07-05 18:51:25 +0530

    feat-124: new feature

--- customer/src/services/customer-service.test.js
+++ customer/src/services/customer-service.test.js
@@ -1,6 +1,6 @@
 // which service it is
 describe("CustomerService", () => { //this CustomerService is the exact class in the customer-service.js
-  // Which function we are testing here
+  // Which function we are testing here, yes testing is imp
   describe("SignIn", () => {
     // Which Scenario we are testing
     test("validate user inputs", () => {});
